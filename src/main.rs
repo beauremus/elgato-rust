fb1e6ac62163e2245bd2e682f9e19b3028dc206b
Beau Harrison <beauharrison@gmail.com>
2023-04-27 00:34:14 -0500

    Refactor before move to interactive CLI.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,23 @@ use mdns_sd::{ServiceDaemon, ServiceEvent};
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
-#[derive(Debug, Serialize_repr, Deserialize_repr)]
+#[derive(Debug, Serialize_repr, Deserialize_repr, Clone, Copy)]
 #[repr(u8)]
 enum Power {
     Off = 0,
     On = 1,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 struct Settings {
     on: Power,
     brightness: u8,
     temperature: u16,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
-struct Command {
+struct LightState {
     number_of_lights: u8,
     lights: Vec<Settings>,
 }
@@ -31,53 +31,94 @@ fn gen_url(address: Ipv4Addr) -> String {
     format!("http://{}:9123/elgato/lights", address)
 }
 
-async fn get_light_status(address: Ipv4Addr) -> Result<Command, reqwest::Error> {
-    let url = gen_url(address);
-    reqwest::get(url).await?.json::<Command>().await
+async fn get_lights_status(addresses: &[Ipv4Addr]) -> Result<Vec<LightState>, reqwest::Error> {
+    let mut statuses: Vec<LightState> = Vec::new();
+
+    for address in addresses.iter().cloned() {
+        // Get the current status of each light
+        // this allows us to fill the struct with the current values
+        // The API requires that we send the entire struct back
+        let url = gen_url(address);
+
+        match reqwest::get(url).await?.json::<LightState>().await {
+            Ok(status) => statuses.push(status),
+            Err(error) => panic!("Problem getting light status: {:?}", error),
+        };
+    }
+
+    Ok(statuses)
 }
 
-#[tokio::main]
-async fn main() {
+fn get_service_addresses(service_type: &str) -> Vec<Ipv4Addr> {
     // Create a daemon
     let mdns: ServiceDaemon = ServiceDaemon::new().expect("Failed to create daemon");
 
     // Browse for a service type.
-    let service_type = "_elg._tcp.local.";
-    let receiver = mdns.browse(service_type).expect("Failed to browse");
     let receiver: mdns_sd::Receiver<ServiceEvent> =
         mdns.browse(service_type).expect("Failed to browse");
 
+    let mut service_found = false;
+    let mut services: Vec<Ipv4Addr> = Vec::new();
     while let Ok(event) = receiver.recv() {
-        if let ServiceEvent::ServiceResolved(info) = event {
-            let addresses = info.get_addresses();
-            if let Some(address) = addresses.iter().next().cloned() {
-                println!("Found Elgato Keylight Air at {}", address);
-
-                // Get the current status of the light
-                // this allows us to fill the struct with the current values
-                // The API requires that we send the entire struct back
-                let mut status: Command = match get_light_status(address).await {
-                    Ok(status) => status,
-                    Err(error) => panic!("Problem getting light status: {:?}", error),
-                };
-                println!("{:?}", status);
-
-                // Toggle the light power state
-                match status.lights[0].on {
-                    Power::On => status.lights[0].on = Power::Off,
-                    Power::Off => status.lights[0].on = Power::On,
-                }
-                println!("Lights toggled: {:?}", status.lights[0].on);
-
-                // Make a PUT request to toggle the light power
-                reqwest::Client::new()
-                    .put(&gen_url(address))
-                    .json(&status)
-                    .send()
-                    .await
-                    .ok();
+        match event {
+            ServiceEvent::ServiceFound(..) => {
+                service_found = true;
             }
-            return;
+            ServiceEvent::ServiceResolved(info) => {
+                let addresses = info.get_addresses();
+                // Append the service address to the list of services
+                if let Some(address) = addresses.iter().next().cloned() {
+                    services.push(address);
+                }
+            }
+            ServiceEvent::SearchStarted(..) => {
+                // If we found a service and we're still searching, we can stop
+                if service_found {
+                    break;
+                }
+            }
+            _ => {}
         }
+    }
+
+    services
+}
+
+async fn update_lights_state(addresses: Vec<Ipv4Addr>, statuses: Vec<LightState>) {
+    for (address, status) in addresses.iter().zip(statuses.iter()) {
+        // Make a PUT request to toggle the light power
+        reqwest::Client::new()
+            .put(&gen_url(*address))
+            .json(&status)
+            .send()
+            .await
+            .ok();
+    }
+}
+
+fn toggle_light_state(statuses: Vec<LightState>) -> Vec<LightState> {
+    let mut local_statuses = statuses;
+
+    for status in local_statuses.iter_mut() {
+        // Toggle the light power state
+        match status.lights[0].on {
+            Power::On => status.lights[0].on = Power::Off,
+            Power::Off => status.lights[0].on = Power::On,
+        }
+    }
+
+    local_statuses
+}
+
+#[tokio::main]
+async fn main() {
+    let addresses = get_service_addresses("_elg._tcp.local.");
+    let statuses = get_lights_status(&addresses).await;
+
+    match statuses {
+        Ok(statuses) => {
+            update_lights_state(addresses, toggle_light_state(statuses)).await;
+        }
+        Err(error) => panic!("Problem getting light status: {:?}", error),
     }
 }
